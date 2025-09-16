fb1635c97e92c0d65d5eace3f4f42fd4c3627684
A. <10308365@coleggwent.ac.uk>
2025-09-16 11:55:46 +0100

    Added cool cup option

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-#Setting cost
+#Setting price
 cost = 3.50
 #Welcoming customer
 name = input("Welcome to the smoothie shop! Whats your name? \n")
-#Asking how many smoothies they would like
+#Asking smoothies
 amount = input(f"Hello {name}! How many smoothies would you like? Each smoothie costs £3.50.\n")
-#Converting all numbers to float and calculating total cost
+#Working out price
 total = float(amount) * float(cost)
-#Outputting customers final smoothie cost
-print(f"Thank you, {name}. The total for your {amount} smoothie(s) will be £{total}")
+#cool cup
+coolcup = input("Would you like a nice cup to keep? It costs £1.\n")
+coolcup = coolcup.lower()
+if coolcup == "yes" or coolcup == "sure" or coolcup == "y":
+    total = total + 1
+else:
+    print("Very well then, suit yourself.")
+  #Show price
+print(f"Thank you, {name}. The total for today will be £{total}")
