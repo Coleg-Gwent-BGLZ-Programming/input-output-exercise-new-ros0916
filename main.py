a2279577b889948bf6d7c6eef30c245967091d15
A. <10308365@coleggwent.ac.uk>
2025-09-16 11:38:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ amount = input(f"Hello {name}! How many smoothies would you like? Each smoothie 
 #Converting all numbers to float and calculating total cost
 total = float(amount) * float(cost)
 #Outputting customers final smoothie cost
-print(f"Thank you, {name}. The total for your {amount} smoothie(s) will be {total}")
+print(f"Thank you, {name}. The total for your {amount} smoothie(s) will be £{total}")
