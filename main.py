11fa56f1d84b3858ec3c00b6d8bc120378071ebd
A. <10308365@coleggwent.ac.uk>
2025-09-16 11:35:50 +0100

    Upload

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-# Starter file for the Smoothie Shop Calculator exercise
-
-print("Welcome to the Smoothie Shop!")
-
-# TODO: Ask the user for their name using input()
-
-
-# TODO: Ask how many smoothies they want to buy
-
-
-# TODO: Convert the number of smoothies to an integer
-
-
-# TODO: Calculate the total cost (each smoothie costs £3.50)
-
-
-# TODO: Display a message using a formatted string
-
-
-# OPTIONAL CHALLENGE:
-# Ask if the customer wants a reusable cup for £1.00 extra
-# Add the cost if they say yes
+#Setting cost
+cost = 3.50
+#Welcoming customer
+name = input("Welcome to the smoothie shop! Whats your name? \n")
+#Asking how many smoothies they would like
+amount = input(f"Hello {name}! How many smoothies would you like? Each smoothie costs £3.50.\n")
+#Converting all numbers to float and calculating total cost
+total = float(amount) * float(cost)
+#Outputting customers final smoothie cost
+print(f"Thank you, {name}. The total for your {amount} smoothie(s) will be {total}")
